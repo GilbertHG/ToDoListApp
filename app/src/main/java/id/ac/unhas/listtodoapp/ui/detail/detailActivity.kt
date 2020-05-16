fa4120ecf1e82ec7fbc70887ac935745f3a7f914
GilbertHG <gilbertgoes11@gmail.com>
2020-05-16 16:03:11 +0800

    Layout of ToDoListApp

---
+++ app/src/main/java/id/ac/unhas/listtodoapp/ui/detail/detailActivity.kt
@@ -0,0 +1,13 @@
+package id.ac.unhas.listtodoapp.ui.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.ac.unhas.listtodoapp.R
+
+class detailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
