bff41e85c22312da7418b5a03a11f2116b2ba2e8
GilbertHG <gilbertgoes11@gmail.com>
2020-05-16 13:02:55 +0800

    Init Commit

---
+++ app/src/main/java/id/ac/unhas/listtodoapp/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.ac.unhas.listtodoapp.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.ac.unhas.listtodoapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
